564ef3c6f0fad43ac54dcb4a8be23c0cbe78ba86
Michael Xusheng Chen <michael.xschen@gmail.com>
2016-11-03 19:34:27 +0800

    webapp

--- clean-all.sh
+++ clean-all.sh
@@ -5,5 +5,11 @@ do
 echo $host
 	ssh "${host}" '
 	echo 931209 | sudo -S rm -rf /var/lib/mesos/*
-	echo 931209 | sudo -S rm -rf /var/log/mesos'
+	echo 931209 | sudo -S rm -rf /var/log/mesos
+	docker rm -f $(docker ps -aq)
+	docker rmi -f $(docker images -aq)'
 done
+./zookeeper-3.4.9/bin/zkCli.sh rmr /mesos
+./zookeeper-3.4.9/bin/zkCli.sh rmr /marathon
+
+
--- docker-webapp/start.sh
+++ docker-webapp/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sed 's/bridged-webapp/bridged-webapp'$RANDOM'/' description.tpl > description.json
-curl -X POST http://202.45.128.160:8080/v2/apps -d @description.json -H "Content-type: application/json"
+curl -X POST http://202.45.128.160:8080/v2/apps -d @description.json -H "Content-type: application/json" | python -m json.tool > result-${RANDOM}.json
